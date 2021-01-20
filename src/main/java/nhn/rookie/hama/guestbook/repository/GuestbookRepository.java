86c5a42a83a2b890e1ee427c8dd73cb2473de963
Noah-43 <ajph03102@naver.com>
2021-01-20 12:20:45 +0900

    Querydsl set #1

--- src/main/java/nhn/rookie/hama/guestbook/repository/GuestbookRepository.java
+++ src/main/java/nhn/rookie/hama/guestbook/repository/GuestbookRepository.java
@@ -2,6 +2,7 @@ package nhn.rookie.hama.guestbook.repository;
 
 import nhn.rookie.hama.guestbook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> { // QuerydslPredicateExecutor 인터페이스 추가 상속(Querydsl)
 }
