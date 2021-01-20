e5d0f243671e99c974c3072147c1ae097590fb89
Noah-43 <ajph03102@naver.com>
2021-01-20 16:49:13 +0900

    목록 처리 테스트 #3

--- src/main/java/nhn/rookie/hama/guestbook/service/GuestbookService.java
+++ src/main/java/nhn/rookie/hama/guestbook/service/GuestbookService.java
@@ -8,7 +8,7 @@ import nhn.rookie.hama.guestbook.entity.Guestbook;
 public interface GuestbookService {
     Long register(GuestbookDTO dto);
 
-    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO); // Request를 받아서 Result를 주는 서비스(헷갈리지 말자)
 
     // Java 8버전부터 인터페이스의 실제 내용을 가지는 코드를 default라는 키워드로 생성할 수 있다.(추상 클래스를 생략 가능)
     default Guestbook dtoToEntity(GuestbookDTO dto) {
--- src/test/java/nhn/rookie/hama/guestbook/service/GuestbookServiceTests.java
+++ src/test/java/nhn/rookie/hama/guestbook/service/GuestbookServiceTests.java
@@ -1,6 +1,9 @@
 package nhn.rookie.hama.guestbook.service;
 
 import nhn.rookie.hama.guestbook.dto.GuestbookDTO;
+import nhn.rookie.hama.guestbook.dto.PageRequestDTO;
+import nhn.rookie.hama.guestbook.dto.PageResultDTO;
+import nhn.rookie.hama.guestbook.entity.Guestbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +24,18 @@ public class GuestbookServiceTests {
                 .build();
 
         System.out.println(service.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
     }
 
 }
